264769d9fc4bd8f9d3d4df5a1d102d8d5f09d232
Mike Brevoort <mike.brevoort@gitpod.io>
2023-12-12 01:41:41 +0000

    incremental commit

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,19 @@
 FUNCTION_NAME="email-ingest"
 ZIP_FILE="fileb://function.zip"
 HANDLER="email_receiver_lambda.lambda_handler"
-RUNTIME="python3.8"
+RUNTIME="python3.11"
 ROLE="arn:aws:iam::463881414897:role/lambda-ex"
 
+# make build directory and copy function there
+rm -rf build
+mkdir -p build
+cp email_receiver_lambda.py build/
+# install dependencies
+pip install -r requirements.txt -t build/
+cd build
+
 #!/bin/bash
-zip -r function.zip email_receiver_lambda.py requirements.txt
+zip -r function.zip email_receiver_lambda.py *
 
 # Check if the Lambda function exists
 if aws lambda get-function --function-name $FUNCTION_NAME 2>/dev/null; then
--- email_receiver_lambda.py
+++ email_receiver_lambda.py
@@ -1,6 +1,16 @@
 import boto3
 import email
 import json
+from openai import OpenAI
+from botocore.exceptions import ClientError
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+client = OpenAI(
+    # This is the default and can be omitted
+    api_key=os.environ.get("OPENAI_API_KEY"),
+)
 
 def lambda_handler(event, context):
     # Initialize the S3 client
@@ -28,15 +38,25 @@ def lambda_handler(event, context):
         print("Subject:", msg['Subject'])
         print("From:", msg['From'])
 
+        body = ""
+
         # Process and print the email body
         if msg.is_multipart():
             for part in msg.walk():
                 if part.get_content_type() == "text/plain":
-                    body = part.get_payload(decode=True)
-                    print("Body:", body.decode())
+                    body = part.get_payload(decode=True).decode()
+                    print("Body:", body)
         else:
-            body = msg.get_payload(decode=True)
-            print("Body:", body.decode())
+            body = msg.get_payload(decode=True).decode()
+            print("Body:", body)
+
+        # Generate completion
+        completion = generate_completion(body)
+        print("Completion:", completion)
+
+        # Send email
+        subject = "Re: " + msg['Subject']
+        send_email(subject, completion, [msg['From']], msg['To'])
 
     except Exception as e:
         print(e)
@@ -46,3 +66,43 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': json.dumps('Email processed')
     }
+
+def generate_completion(text):
+    response = client.chat.completions.create(
+    model="gpt-4-1106-preview",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": text},
+    ])
+
+    return response.choices[0].message.content
+
+
+def send_email(subject, body, to_addresses, from_address, aws_region="us-west-2"):
+    # Create a new SES client
+    client = boto3.client('ses', region_name=aws_region)
+
+    try:
+        # Provide the contents of the email.
+        response = client.send_email(
+            Destination={
+                'ToAddresses': to_addresses,
+            },
+            Message={
+                'Body': {
+                    'Text': {
+                        'Charset': 'UTF-8',
+                        'Data': body,
+                    },
+                },
+                'Subject': {
+                    'Charset': 'UTF-8',
+                    'Data': subject,
+                },
+            },
+            Source=from_address,
+        )
+    except ClientError as e:
+        print(f"Error sending email: {e.response['Error']['Message']}")
+    else:
+        print(f"Email sent! Message ID: {response['MessageId']}")
